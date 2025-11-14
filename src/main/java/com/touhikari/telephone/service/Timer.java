604e83fd05e86fca5c78aa8ac14b5e9de96b489d
TouHikari <tang20051026@foxmail.com>
2025-11-14 17:49:10 +0800

    feat: Timer

--- src/main/java/com/touhikari/telephone/service/Timer.java
+++ src/main/java/com/touhikari/telephone/service/Timer.java
@@ -2,23 +2,37 @@ package com.touhikari.telephone.service;
 
 public class Timer {
 
-    public void reset() {
+    private long startAt;
+    private long lastElapsed;
+    private boolean running;
 
+    public void reset() {
+        running = false;
+        startAt = 0L;
+        lastElapsed = 0L;
     }
 
     public void start() {
-
+        startAt = System.nanoTime();
+        running = true;
     }
 
     public void stop() {
-
+        if (running) {
+            lastElapsed = (System.nanoTime() - startAt) / 1_000_000L;
+            running = false;
+        }
     }
 
     public int elapsed() {
-
+        if (running) {
+            long ms = (System.nanoTime() - startAt) / 1_000_000L;
+            return (int) ms;
+        }
+        return (int) lastElapsed;
     }
 
     public boolean timeout(int ms) {
-
+        return elapsed() >= ms;
     }
 }
